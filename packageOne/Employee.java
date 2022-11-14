7e0d9034dc894832d614202b281d81020f1a0af1
Sarge_pro <0938157780@ukr.net>
2022-11-14 18:39:40 +0200

    1

---
+++ packageOne/Employee.java
@@ -0,0 +1,62 @@
+package packageOne;
+
+public class Employee {
+    private String fullName = "FIDO";
+    private String position = "trainee";
+    private String email = "trainee@gmail.com";
+    private String phoneNumber = "+380505554433";
+    private int age = 33;
+
+
+    public Employee(String fullName, String position, String email, String phoneNumber, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public Employee() {
+
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ packageOne/Main.java
@@ -0,0 +1,20 @@
+package packageOne;
+
+import packageOne.Employee;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee trainee = new Employee();
+
+//        System.out.println(trainee.getFullName());
+//        System.out.println(trainee.getPosition());
+//        System.out.println(trainee.getEmail());
+//        System.out.println(trainee.getPhoneNumber());
+//        System.out.println(trainee.getAge());
+
+        Car lada = new Car();
+
+        lada.start();
+    }
+}
